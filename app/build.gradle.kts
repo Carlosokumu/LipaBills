c4fe834ff695539fa2dd8190f46f5e608c918ae8
Carlos <carlosokumu254@gmail.com>
2022-12-18 04:03:49 -0500

    [UPDATE]Migrated Gradle buildScript to Kotlin dsl

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
+
+
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
+//    id 'com.android.application'
+//    id 'org.jetbrains.kotlin.android'
+    id(libs.plugins.android.application.get().pluginId)
+    id(libs.plugins.kotlin.android.get().pluginId)
+    id(libs.plugins.kotlin.kapt.get().pluginId)
 }
 
 android {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "LipaKeja"
-include ':app'
+include(":app")
