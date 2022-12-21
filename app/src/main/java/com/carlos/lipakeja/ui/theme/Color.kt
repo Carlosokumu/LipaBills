01c58c174211bb7cf01195cec221fe7ba31ce2cb
Carlos <carlosokumu254@gmail.com>
2022-12-21 02:06:33 -0500

    [ADD]Added LipaKeja Colors

--- app/src/main/java/com/carlos/lipakeja/ui/theme/Color.kt
+++ app/src/main/java/com/carlos/lipakeja/ui/theme/Color.kt
@@ -2,7 +2,18 @@ package com.carlos.lipakeja.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
+
+/*
+    LipaKeja Colors
+*/
+
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val BackgroundMain = Color(0xFFFFEEEE)
+val WhiteMain = Color(0xFFFCFAFA)
+val TopBarColor = Color(0xFF9C9C96)
+val ButtonColor = Color(0xFFAEABAB)
