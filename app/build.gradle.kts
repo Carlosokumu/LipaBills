12ef0a8176eb1a811883e6fb28d6b81771367fdb
Carlos <carlosokumu254@gmail.com>
2022-12-21 02:07:18 -0500

    [UPDATE]Configured Apps gradle to kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,59 +9,118 @@ plugins {
 }
 
 android {
-    compileSdk 32
+
+    compileSdk = Configuration.compileSdk
 
     defaultConfig {
-        applicationId "com.carlos.lipakeja"
-        minSdk 21
-        targetSdk 32
-        versionCode 1
-        versionName "1.0"
+        applicationId  = "com.carlos.lipakeja"
+        minSdk = Configuration.minSdk
+        targetSdk = Configuration.targetSdk
+        versionCode = Configuration.versionCode
+        versionName = Configuration.versionName
+        vectorDrawables.useSupportLibrary = true
 
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary true
-        }
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
+//     compileSdk 32
+//
+//    defaultConfig {
+//        applicationId "com.carlos.lipakeja"
+//        minSdk 21
+//        targetSdk 32
+//        versionCode 1
+//        versionName "1.0"
+//
+//        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+//        vectorDrawables {
+//            useSupportLibrary true
+//        }
+//    }
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+//    buildTypes {
+//        release {
+//            minifyEnabled false
+//            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+//        }
+//    }
+
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
+//    compileOptions {
+//        sourceCompatibility JavaVersion.VERSION_1_8
+//        targetCompatibility JavaVersion.VERSION_1_8
+//    }
+
     kotlinOptions {
-        jvmTarget = '1.8'
+        //jvmTarget = '1.8'
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        freeCompilerArgs = listOf("-Xjvm-default=all")
     }
+//    kotlinOptions {
+//        jvmTarget = '1.8'
+//    }
+//    buildFeatures {
+//        compose true
+//    }
     buildFeatures {
-        compose true
+        compose = true
     }
+//    composeOptions {
+//        kotlinCompilerExtensionVersion compose_version
+//    }
     composeOptions {
-        kotlinCompilerExtensionVersion compose_version
+        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
     }
+//    packagingOptions {
+//        resources {
+//            excludes += '/META-INF/{AL2.0,LGPL2.1}'
+//        }
+//    }
     packagingOptions {
         resources {
-            excludes += '/META-INF/{AL2.0,LGPL2.1}'
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
 
 dependencies {
 
-    implementation 'androidx.core:core-ktx:1.7.0'
-    implementation "androidx.compose.ui:ui:$compose_version"
-    implementation "androidx.compose.material:material:$compose_version"
-    implementation "androidx.compose.ui:ui-tooling-preview:$compose_version"
-    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.3.1'
-    implementation 'androidx.activity:activity-compose:1.3.1'
-    testImplementation 'junit:junit:4.13.2'
-    androidTestImplementation 'androidx.test.ext:junit:1.1.4'
-    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.0'
-    androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_version"
-    debugImplementation "androidx.compose.ui:ui-tooling:$compose_version"
-    debugImplementation "androidx.compose.ui:ui-test-manifest:$compose_version"
+    //implementation 'androidx.core:core-ktx:1.7.0'
+    implementation(libs.androidx.ktx)
+    implementation(libs.androidx.material.compose)
+    implementation(libs.androidx.compose.ui)
+   // implementation "androidx.compose.ui:ui:$compose_version"
+   // implementation "androidx.compose.material:material:$compose_version"
+
+   // implementation "androidx.compose.ui:ui-tooling-preview:$compose_version"
+    implementation(libs.androidx.compose.tooling)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.compose.activity)
+//    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.3.1'
+//    implementation 'androidx.activity:activity-compose:1.3.1'
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso)
+
+    //testImplementation 'junit:junit:4.13.2'
+   // androidTestImplementation 'androidx.test.ext:junit:1.1.4'
+  //  androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.0'
+    androidTestImplementation(libs.androidx.compose.junit)
+    //androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_version"
+   // debugImplementation "androidx.compose.ui:ui-tooling:$compose_version"
+    debugImplementation(libs.androidx.compose.tooling)
+    debugImplementation(libs.androidx.compose.manifest)
+
+    //debugImplementation "androidx.compose.ui:ui-test-manifest:$compose_version"
 }
