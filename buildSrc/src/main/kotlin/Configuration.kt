37e28f0f07118a785c4d3555fab2cfde8f7ace1e
Carlos <carlosokumu254@gmail.com>
2022-12-21 04:07:28 -0500

    [ADD]Added a configuration file

---
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -0,0 +1,9 @@
+
+
+object  Configuration{
+    const val compileSdk = 33
+    const val targetSdk = 33
+    const val minSdk = 23
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
