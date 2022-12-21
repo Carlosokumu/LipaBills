c1be300836ed001b4c189b5d22c01dbe05eeb768
Carlos <carlosokumu254@gmail.com>
2022-12-21 04:07:00 -0500

    [UPDATE]Clean up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,6 @@
 
 
 plugins {
-//    id 'com.android.application'
-//    id 'org.jetbrains.kotlin.android'
     id(libs.plugins.android.application.get().pluginId)
     id(libs.plugins.kotlin.android.get().pluginId)
     id(libs.plugins.kotlin.kapt.get().pluginId)
@@ -25,67 +23,35 @@ android {
 
     }
 
-//     compileSdk 32
-//
-//    defaultConfig {
-//        applicationId "com.carlos.lipakeja"
-//        minSdk 21
-//        targetSdk 32
-//        versionCode 1
-//        versionName "1.0"
-//
-//        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
-//        vectorDrawables {
-//            useSupportLibrary true
-//        }
-//    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-//    buildTypes {
-//        release {
-//            minifyEnabled false
-//            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
-//        }
-//    }
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-//    compileOptions {
-//        sourceCompatibility JavaVersion.VERSION_1_8
-//        targetCompatibility JavaVersion.VERSION_1_8
-//    }
+
 
     kotlinOptions {
         //jvmTarget = '1.8'
         jvmTarget = JavaVersion.VERSION_1_8.toString()
         freeCompilerArgs = listOf("-Xjvm-default=all")
     }
-//    kotlinOptions {
-//        jvmTarget = '1.8'
-//    }
-//    buildFeatures {
-//        compose true
-//    }
+
     buildFeatures {
         compose = true
     }
-//    composeOptions {
-//        kotlinCompilerExtensionVersion compose_version
-//    }
+
     composeOptions {
         kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
     }
-//    packagingOptions {
-//        resources {
-//            excludes += '/META-INF/{AL2.0,LGPL2.1}'
-//        }
-//    }
+
     packagingOptions {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -95,32 +61,20 @@ android {
 
 dependencies {
 
-    //implementation 'androidx.core:core-ktx:1.7.0'
+
     implementation(libs.androidx.ktx)
     implementation(libs.androidx.material.compose)
     implementation(libs.androidx.compose.ui)
-   // implementation "androidx.compose.ui:ui:$compose_version"
-   // implementation "androidx.compose.material:material:$compose_version"
-
-   // implementation "androidx.compose.ui:ui-tooling-preview:$compose_version"
     implementation(libs.androidx.compose.tooling)
     implementation(libs.androidx.lifecycle)
     implementation(libs.androidx.compose.activity)
-//    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.3.1'
-//    implementation 'androidx.activity:activity-compose:1.3.1'
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso)
-
-    //testImplementation 'junit:junit:4.13.2'
-   // androidTestImplementation 'androidx.test.ext:junit:1.1.4'
-  //  androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.0'
     androidTestImplementation(libs.androidx.compose.junit)
-    //androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_version"
-   // debugImplementation "androidx.compose.ui:ui-tooling:$compose_version"
     debugImplementation(libs.androidx.compose.tooling)
     debugImplementation(libs.androidx.compose.manifest)
 
-    //debugImplementation "androidx.compose.ui:ui-test-manifest:$compose_version"
 }
