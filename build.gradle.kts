44d25b76ce9b60590b59f270027a74f47d2fea6b
Carlos <carlosokumu254@gmail.com>
2023-02-20 08:20:04 -0500

    [ADD]Config: added configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,22 @@
+
 buildscript {
-    ext {
-        //compose_version = '1.1.0-beta01'
-        val compose_version by extra("1.1.0-beta01")
-        val kotlin_version by extra("1.5.31")
+
+    val compose_version by extra("1.1.1")
+    val kotlin_version by extra("1.6.10")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath(libs.agp)
+        classpath(libs.kotlin.gradlePlugin)
+        //classpath(libs.hilt.plugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-//plugins {
-//    id 'com.android.application' version '7.2.2' apply false
-//    id 'com.android.library' version '7.2.2' apply false
-//    id 'org.jetbrains.kotlin.android' version '1.5.31' apply false
-//}
-repositories {
-    google()
-    mavenCentral()
-    gradlePluginPortal()
-}
 
 
 
-
-dependencies {
-
-    classpath(libs.agp)
-    classpath(libs.kotlin.gradlePlugin)
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-}
